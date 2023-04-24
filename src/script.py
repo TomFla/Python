baef7997afca9aa543314c9c4d95565b7db51937
Tomasz Flakus <tomasz@localhost.localdomain>
2023-04-24 21:14:29 +0200

    imputs and if statements

--- src/script.py
+++ src/script.py
@@ -1,1 +1,14 @@
+# comment
 print("Hello World")
+print("I'm new to Python")
+
+
+_age = int(input('Podaj wiek: '))
+# print(_age)
+
+if _age < 18:
+    print("jestes niepelnoletni")
+elif _age >=18 and _age <=100:
+    print("Witaj")
+else:
+    print("Ludzie tyle nie zyja")
